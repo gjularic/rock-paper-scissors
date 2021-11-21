1fa5b5b7cae17c2f3d3d59dcb0583c78aefd7687
gjularic <gvozdan.jularic@gmail.com>
2021-11-21 18:55:04 +0000

    Set up main variables in js file and select elements in DOM

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Set up main variables and selected elements in DOM
+
+let playerGameScore = document.getElementById("playerScore");
+let computerGameScore = document.getElementById("computerScore");
+const selectOption = document.querySelectorAll(".clickOption");
+let gameResultShow = document.getElementById("result");
