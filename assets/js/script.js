cc08cb07562abfbf1b2dc4d777e17bd05b90cc1c
gjularic <gvozdan.jularic@gmail.com>
2021-11-21 19:59:10 +0000

    Add event listener function to check which option has been selected by the player

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,30 @@ let playerGameScore = document.getElementById("playerScore");
 let computerGameScore = document.getElementById("computerScore");
 const selectOption = document.querySelectorAll(".clickOption");
 let gameResultShow = document.getElementById("result");
+
+/*
+Function to add event listeners
+to each option selected by the player
+*/
+for (let option of selectOption) {
+    option.addEventListener("click", function(event) {
+
+        /*
+        Used this option when I noticed that parentNode
+        needs to be selected in order to get the id-s
+        */
+        playerOption = event.target.parentNode.id;
+
+        /*
+        playerOption = event.target.className;
+        if (playerOption === "fas fa-hand-rock") {
+            playerOption = 0;
+        } else if (playerOption === "fas fa-hand-paper") {
+            playerOption = 1;
+        } else {
+            playerOption = 2;
+        }
+        */
+        console.log(playerOption);
+    })
+}
